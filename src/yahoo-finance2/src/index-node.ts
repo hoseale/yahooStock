b5f18abf05550f171f19b148f2e0f6f2c30452c9
hosea <lhl1990s@163.com>
2024-06-14 18:07:45 +0800

    bug1

--- src/yahoo-finance2/src/index-node.ts
+++
@@ -1,8 +0,0 @@
-import yahooFinance from "./index-common";
-import nodeEnvironment from "./env-node";
-import { ExtendedCookieJar } from "./lib/cookieJar";
-
-yahooFinance._env = nodeEnvironment;
-
-export { ExtendedCookieJar };
-export default yahooFinance;
